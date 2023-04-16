e6008dd8d42a2135c4021470657ac39b07435beb
m.backi <backimichal@gmail.com>
2023-04-16 17:40:01 +0200

    Update switchPage.js

--- src/js/modules/switchPage.js
+++ src/js/modules/switchPage.js
@@ -1,3 +1,13 @@
 import { fetchItems } from './fetchItems';
 import { displayItems } from './displayItems';
 //
+function switchPage(pageNumber, query) {
+  fetchItems(pageNumber, query)
+    .then(items => {
+      displayItems(items);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+switchPage(1, 'popular');
