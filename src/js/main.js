fd4af6fc86d0a2e0d3dab4222e1536b538423abe
SebastianZasadni <emblelele@gmail.com>
2023-04-17 17:22:05 +0200

    fix fetchItems

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,2 @@
 import 'modern-normalize/modern-normalize.css';
 import '../sass/main.scss';
-
-import {fetchTrendingMovies,fetchMoviesBySearchQuery} from './modules/fetchItems';
-fetchMoviesBySearchQuery('60')
-fetchTrendingMovies();
