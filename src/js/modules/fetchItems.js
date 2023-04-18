5f1e0a49b34408268cd281b339d3343fe96c5ef3
SebastianZasadni <emblelele@gmail.com>
2023-04-18 09:22:51 +0200

    fix fetchItems

--- src/js/modules/fetchItems.js
+++ src/js/modules/fetchItems.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_KEY = '1c8f6b064eb2a1f6bd306bc5a0f759ec';
+const API_KEY = 'api_key=1c8f6b064eb2a1f6bd306bc5a0f759ec';
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_LANGUAGE = 'language=en-US';
 
 export const fetchTrendingMovies = async () => {
   try {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`,
-    );
+    const { data } = await axios.get(`${API_URL}trending/all/day?${API_KEY}&${API_LANGUAGE}`);
     return data; // => {page, results, total_pages, total_results}
   } catch (error) {
     console.log(error.message);
@@ -16,7 +16,7 @@ export const fetchTrendingMovies = async () => {
 export const fetchMoviesBySearchQuery = async (searchQuery, page = 1) => {
   try {
     const { data } = await axios.get(
-      ` https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&page=${page}&query=${searchQuery}`,
+      `${API_URL}search/movie?${API_KEY}&${API_LANGUAGE}page=${page}&query=${searchQuery}`,
     );
     if (data.results.length > 0) {
       return data; // => {page, results, total_pages, total_results}
