228b6cf36c7085a43bfcd527434726d9c9d9da69
Natalia Grela <nataliagrela83@mail.com>
2023-04-18 21:44:34 +0200

    feature/displayItems

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,5 @@
 import 'modern-normalize/modern-normalize.css';
 import '../sass/main.scss';
+
+import { renderMovies } from './modules/displayItems';
+
---
+++ src/js/modules/displayItems.js
@@ -0,0 +1,29 @@
+import { changeGenresIdToName } from './changeGenresIdToName';
+
+import { fetchTrendingMovies } from './fetchItems';
+
+(async function () {
+  const movies = await fetchTrendingMovies();
+  const results = movies.results;
+  let moviesHTML = await Promise.all(
+    results.map(async item => {
+      let { genre_ids, title, poster_path, vote_average, release_date, id } = item;
+
+      const genresNames = await changeGenresIdToName(genre_ids);
+
+      return `<li id=${id} class="movie-item">
+    <img class="movie-item__poster" src="https://image.tmdb.org/t/p/w500/${poster_path}"/>
+    <div class="movie-item__details">
+      <h2 class="movie-item__title">${title}</h2>
+      <span class="movie-item__genre">${genresNames.join(', ')}</span>
+      <span class="movie-item__line">|</span>
+      <span class="movie-item__year">Year: ${release_date}</span>
+      <span class="movie-item__rating">${vote_average}</span>
+    </div>
+  </li>`;
+    }),
+  );
+  moviesHTML = moviesHTML.join(' ');
+  const moviesWrapper = document.querySelector('.main-content__list');
+  moviesWrapper.innerHTML = moviesHTML;
+})();
