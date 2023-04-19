d4681aa6d81a3039ff81d598ed14dfee376c3d32
rafallakomiec <114759971+rafallakomiec@users.noreply.github.com>
2023-04-19 16:50:16 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import 'modern-normalize/modern-normalize.css';
 import '../sass/main.scss';
 
+import { fetchTrendingMovies } from './fetchItems';
 import { renderMovies } from './modules/displayItems';
+
+const movies = fetchTrendingMovies();
+renderMovies(movies);
 
