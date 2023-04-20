d286b2a199be853350312ef4c5fde1ea0a0cf1ab
Tomasz Ostaszewski <114577269+OstTomasz@users.noreply.github.com>
2023-04-20 19:29:04 +0200

    clean-up
    js:
    - wprowadzenie poprawek od Mikołaja: usunięcie dead code, refs w funkcji exportowej, dodano funkcję qs, zmiana funkcji toggleModal na strzałkową
    - dodanie odnośników do wartości edytowalnych
    - ujednolicenie nazewnictwa
    
    html:
    - dodano id w miejscach edytowanych przez js
    - zmieniono wartości edytowalnych przez js tak, aby było wiadomo, że na pewno się załadowały poprawne
    scss:
    - poprawienie wyświetlania modala
    - modal można scrollować (przydatne przy niższych wyświetlaczach)
    - modal wyświetla się na samej górze (wcześniej footer go przykrywał)

--- src/js/modules/openMovieModal.js
+++ src/js/modules/openMovieModal.js
@@ -1,16 +1,26 @@
-// import { fetchItems } from './fetchItems';
+// const filmTitle=document.querySelector
 
 export const openMovieModal = () => {
+  const qs = s => document.querySelector(s);
+  const toggleModal = () => refs.modal.classList.toggle('is-hidden');
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    modal: qs('[data-modal]'),
+    //modal description refs
+    modalFilmPoster: qs('#modal-poster'),
+    modalFilmTitle: qs('#modal-film-title'),
+    modalVoteAverage: qs('#modal-vote-average'),
+    modalVoteTotal: qs('#modal-votes-total'),
+    modalPopularity: qs('#modal-votes-total'),
+    modalOriginalTitle: qs('#modal-original-title'),
+    modalGenre: qs('#modal-genre'),
+    modalAbout: qs('#modal-about'),
+    //buttons
+    modalBtnWatched: qs('#modal-button-watched'),
+    modalBtnQueue: qs('#modal-button-queue'),
+    modalBtnOpen: qs('[data-modal-open]'),
+    modalBtnClose: qs('[data-modal-close]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
+  refs.modalBtnOpen.addEventListener('click', toggleModal);
+  refs.modalBtnClose.addEventListener('click', toggleModal);
 };
