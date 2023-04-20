66ddc0c5b190796fe4474a50e66b35a4850baa96
SebastianZasadni <emblelele@gmail.com>
2023-04-20 22:08:58 +0200

    feature_searchInput

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,31 @@ import 'modern-normalize/modern-normalize.css';
 import '../sass/main.scss';
 
 import { openMovieModal } from './modules/openMovieModal.js';
-import { fetchTrendingMovies } from './modules/fetchItems';
+import { fetchTrendingMovies, fetchMoviesBySearchQuery } from './modules/fetchItems';
 import { renderMovies } from './modules/displayItems';
-
-( async () => {
-  const movies = await fetchTrendingMovies();
-  renderMovies(movies);
-})();
-
 openMovieModal();
+window.addEventListener('load', async event => {
+  event.preventDefault();
+  try {
+    const movies = await fetchTrendingMovies(1);
+    await renderMovies(movies);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+const searchInput = document.querySelector('.header__form-input');
+const searchButton = document.querySelector('.header__form-btn');
+
+searchButton.addEventListener('click', async e => {
+  e.preventDefault();
+  const movieTitle = searchInput.value.trim();
+  try {
+    const movies = await fetchMoviesBySearchQuery(movieTitle, 1);
+    await renderMovies(movies);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+
